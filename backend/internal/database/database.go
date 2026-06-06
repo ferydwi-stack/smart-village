fe085cee30399bd92ff096dadf181a4a48c11163
fery dwi ramadhi <ferydwir27@gmail.com>
2026-06-06 15:27:13 +0700

    fix: change postgres sslmode to require for neon db

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -21,7 +21,7 @@ func Connect(cfg *config.Config) error {
 	var err error
 
 	// 1. Connect to PostgreSQL
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
 	
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
